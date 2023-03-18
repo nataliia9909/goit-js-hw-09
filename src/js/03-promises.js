e5c702595fca5d719b3c3f18f754249c71ffd627
nataliia9909 <113458747+nataliia9909@users.noreply.github.com>
2023-03-18 21:46:22 +0300

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+
+  const { delay, step, amount } = evt.target.elements;
+
+  let curentDelay = Number(delay.value);
+
+  for (let i = 1; i <= Number(amount.value); i += 1) {
+    createPromise(i, curentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    curentDelay += Number(step.value);
+}
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+
+return new Promise((resolve, reject) => {
+  setTimeout(() => {
+ if (shouldResolve) {
+    resolve ({ position, delay })
   } else {
-    // Reject
+    reject ({ position, delay })
   }
+  }, delay)
+})
+
+
+  
+ 
 }
